fc149f634f026f4eec10d08a78697590964ebb6b
Tony Nguyen <tony.nguyen@smail.th-koeln.de>
2023-02-15 14:37:35 +0100

    Hauptmenu added

---
+++ BarcodeOwl/Assets/OpenScanner.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenScanner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void startScan()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+
+}
