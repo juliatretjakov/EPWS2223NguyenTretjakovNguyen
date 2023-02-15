ff91c258bd5a86e2dc6b318199ef064b2824cd68
Tony Nguyen <tony.nguyen@smail.th-koeln.de>
2023-02-15 11:56:03 +0100

    UnityFiles added

---
+++ BarcodeOwl/Assets/Samples/Simple/SimpleDemo.cs
@@ -0,0 +1,151 @@
+﻿using BarcodeScanner;
+using BarcodeScanner.Scanner;
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Wizcorp.Utils.Logger;
+using System.Net;
+using System.IO;
+
+public class SimpleDemo : MonoBehaviour {
+
+	private IScanner BarcodeScanner;
+	public Text TextHeader;
+	public Text EcoScore;
+	public RawImage Image;
+	public AudioSource Audio;
+	public string openFoodFacts ="https://world.openfoodfacts.org/api/v2/product/";
+
+	// Disable Screen Rotation on that screen
+	void Awake()
+	{
+		Screen.autorotateToPortrait = false;
+		Screen.autorotateToPortraitUpsideDown = false;
+	}
+
+	void Start () {
+		// Create a basic scanner
+		BarcodeScanner = new Scanner();
+		BarcodeScanner.Camera.Play();
+
+		// Display the camera texture through a RawImage
+		BarcodeScanner.OnReady += (sender, arg) => {
+			// Set Orientation & Texture
+			Image.transform.localEulerAngles = BarcodeScanner.Camera.GetEulerAngles();
+			Image.transform.localScale = BarcodeScanner.Camera.GetScale();
+			Image.texture = BarcodeScanner.Camera.Texture;
+
+			// Keep Image Aspect Ratio
+			var rect = Image.GetComponent<RectTransform>();
+			var newHeight = rect.sizeDelta.x * BarcodeScanner.Camera.Height / BarcodeScanner.Camera.Width;
+			rect.sizeDelta = new Vector2(rect.sizeDelta.x, newHeight);
+		};
+
+		// Track status of the scanner
+		BarcodeScanner.StatusChanged += (sender, arg) => {
+			TextHeader.text = "Status: " + BarcodeScanner.Status;
+		};
+	}
+
+	/// <summary>
+	/// The Update method from unity need to be propagated to the scanner
+	/// </summary>
+	void Update()
+	{
+		if (BarcodeScanner == null)
+		{
+			return;
+		}
+		BarcodeScanner.Update();
+	}
+
+	#region UI Buttons
+
+	public void ClickStart()
+	{
+		if (BarcodeScanner == null)
+		{
+			Log.Warning("No valid camera - Click Start");
+			return;
+		}
+
+		//GetScores("5411188116905"); //manuelle Eingabe simulieren
+		GetScores("54111881169051561"); //Falsche Eingabe
+
+		// Start Scanning
+		BarcodeScanner.Scan((barCodeType, barCodeValue) => {
+			BarcodeScanner.Stop();
+			TextHeader.text = "Found: " + barCodeType + " / " + barCodeValue;
+
+			// Feedback
+			Audio.Play();
+
+			#if UNITY_ANDROID || UNITY_IOS
+			Handheld.Vibrate();
+			#endif
+
+			GetScores(barCodeValue);
+			
+		});
+
+	}
+
+	public void GetScores(string barCodeValue){
+		try {
+			//webReq.url=string.Format= openfoodfacts+barCodeValue+"/?fields=product_name,nutriscore_grade,ecoscore_grade";
+			HttpWebRequest request= (HttpWebRequest)WebRequest.Create(openFoodFacts+barCodeValue+"/?fields=product_name,nutriscore_grade,ecoscore_grade");
+			HttpWebResponse response=(HttpWebResponse)request.GetResponse();
+			if(response.StatusCode==HttpStatusCode.OK){
+				StreamReader reader = new StreamReader(response.GetResponseStream());
+				EcoScore.text = reader.ReadToEnd();	
+			}
+			response.Close();
+		}catch(WebException e){
+            Debug.Log((int)((HttpWebResponse)e.Response).StatusCode);
+			EcoScore.text= string.Format("{0}",(int)((HttpWebResponse)e.Response).StatusCode);
+        }
+	}
+
+	public void ClickStop()
+	{
+		if (BarcodeScanner == null)
+		{
+			Log.Warning("No valid camera - Click Stop");
+			return;
+		}
+
+		// Stop Scanning
+		BarcodeScanner.Stop();
+	}
+
+	public void ClickBack()
+	{
+		// Try to stop the camera before loading another scene
+		StartCoroutine(StopCamera(() => {
+			SceneManager.LoadScene("Boot");
+		}));
+	}
+
+	/// <summary>
+	/// This coroutine is used because of a bug with unity (http://forum.unity3d.com/threads/closing-scene-with-active-webcamtexture-crashes-on-android-solved.363566/)
+	/// Trying to stop the camera in OnDestroy provoke random crash on Android
+	/// </summary>
+	/// <param name="callback"></param>
+	/// <returns></returns>
+	public IEnumerator StopCamera(Action callback)
+	{
+		// Stop Scanning
+		Image = null;
+		BarcodeScanner.Destroy();
+		BarcodeScanner = null;
+
+		// Wait a bit
+		yield return new WaitForSeconds(0.1f);
+
+		callback.Invoke();
+	}
+
+	#endregion
+}
